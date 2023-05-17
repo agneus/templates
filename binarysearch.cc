8f96624ee918068439a49d8a960836c7d3b2cba3
Agneus <saminshararnafi@gmail.com>
2023-05-16 21:40:20 -0400

    sqrt

--- binarysearch.cc
+++ binarysearch.cc
@@ -15,12 +15,34 @@ int Lower_bound(vector<int a, int target) {
     int ans = -1;
     while(l < r) {
         int m = l + (r - l)/2;
-        if(a[m] >= target) {
+        if(a[m] >= target) { // condition for true
             ans = m;
-            r = m - 1;
+            r = m - 1; // better value
         }
         else{
-            l = m - 1;
+            l = m - 1; // make it true
         }
     }
+}
+
+// rotated array
+
+
+
+// find peak
+
+
+// sqrt
+int sqrt(int x, int pres) {
+    int l = 0; r = x;
+    while(r - l > pres) {
+        int m = l + (r - l)/2;
+        if( m * m < x) {
+            l = m;
+        }
+        else {
+            r = m;
+        }
+    }
+    return l + (r - l)/2;
 }
